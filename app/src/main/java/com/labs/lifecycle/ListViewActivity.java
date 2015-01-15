88820c170718a4ef7451f07b9d6633fb7b0e3b78
Pablo PHG <pablophg33@gmail.com>
2015-01-15 13:08:46 +0100

    Added more widgets

--- app/src/main/java/com/labs/lifecycle/ListViewActivity.java
+++ app/src/main/java/com/labs/lifecycle/ListViewActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+//import android.widget.ListView;
 
 
 public class ListViewActivity extends ListActivity {
 
-    private ListView lv;
+    //private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
