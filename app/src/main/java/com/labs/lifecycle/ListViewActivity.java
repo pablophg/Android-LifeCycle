3c22cba2263e11a3deb447a5604bf157149c71e7
Pablo PHG <pablophg33@gmail.com>
2015-01-15 11:26:30 +0100

    Added List View Activity

---
+++ app/src/main/java/com/labs/lifecycle/ListViewActivity.java
@@ -0,0 +1,49 @@
+package com.labs.lifecycle;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class ListViewActivity extends ListActivity {
+
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //lv = (ListView) findViewById(R.id.lv_prueba);
+
+        String[] s = getResources().getStringArray(R.array.OpcionesLista);
+
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, s));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
